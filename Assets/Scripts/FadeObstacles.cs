72926eef91c07391a678770a40388f10d2d97ccd
Wessel Frijters <wessel.frijters@gmail.com>
2018-10-10 19:13:46 +0200

    Alpha version 0.1

---
+++ Assets/Scripts/FadeObstacles.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeObstacles : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Contains("Obstacle"))
+        {
+            other.gameObject.GetComponent<Obstacle>().Fade();
+        }
+    }
+}
+    
