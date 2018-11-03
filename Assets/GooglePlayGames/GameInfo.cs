9bbc356810934188fb1eb01b00ab8e6511b17896
Wessel Frijters <wessel.frijters@gmail.com>
2018-11-03 15:49:07 +0100

    Stable Version

--- Assets/GooglePlayGames/GameInfo.cs
+++ Assets/GooglePlayGames/GameInfo.cs
@@ -38,7 +38,7 @@ namespace GooglePlayGames {
 
         public const string ApplicationId = "1002817345117"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = "1002817345117-3k52f5405si6nc3m33km4a8dkk5k41hb.apps.googleusercontent.com"; // Filled in automatically
+        public const string WebClientId = ""; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
